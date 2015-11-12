e218ec94a9fc629905522695a7c6089d26666699
SonicGD <sonicgd@gmail.com>
2015-11-12 17:43:20 +0500

    sitelinks validation

--- services/sitelinks/models/Sitelink.php
+++ services/sitelinks/models/Sitelink.php
@@ -2,20 +2,34 @@
 
 namespace directapi\services\sitelinks\models;
 
+use directapi\components\constraints as DirectApiAssert;
+use Symfony\Component\Validator\Constraints as Assert;
+
 class Sitelink
 {
     /**
      * @var string
+     * @Assert\NotBlank()
+     * @Assert\Length(
+     *     max=30
+     * )
      */
     public $Title;
 
     /**
      * @var string
+     * @Assert\NotBlank()
+     * @Assert\Length(
+     *     max=1024
+     * )
      */
     public $Href;
 
     /**
      * @var string
+     * @Assert\Length(
+     *     max=60
+     * )
      */
     public $Description;
 }
--- services/sitelinks/models/SitelinksSetAddItem.php
+++ services/sitelinks/models/SitelinksSetAddItem.php
@@ -2,10 +2,35 @@
 
 namespace directapi\services\sitelinks\models;
 
-class SitelinksSetAddItem
+use directapi\components\constraints as DirectApiAssert;
+use directapi\components\interfaces\ICallbackValidation;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
+
+class SitelinksSetAddItem implements ICallbackValidation
 {
     /**
-     * @var Sitelink
+     * @var Sitelink[]
+     * @DirectApiAssert\ArrayOf(type="\directapi\services\sitelinks\modelsSitelink")
+     * @Assert\Count(
+     *     min=1,
+     *     max=4
+     * )
      */
     public $Sitelinks;
+
+    /**
+     * @Assert\Callback()
+     * @param ExecutionContextInterface $context
+     */
+    public function isValid(ExecutionContextInterface $context)
+    {
+        $length = 0;
+        foreach ($this->Sitelinks as $link) {
+            $length += mb_strlen($link->Title);
+        }
+        if ($length > 66) {
+            $context->buildViolation("Суммарная длина текстов всех быстрых ссылок в наборе — не более 66 символов")->atPath('Sitelink')->addViolation();
+        }
+    }
 }
