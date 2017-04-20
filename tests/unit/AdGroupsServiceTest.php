d62d59315c07f8985cfd465b28eb90e53cc5b626
KateAAAA <a.katyxa@mail.ru>
2017-04-20 16:26:32 +0500

    GroupsTest

--- tests/unit/AdGroupsServiceTest.php
+++ tests/unit/AdGroupsServiceTest.php
@@ -3,36 +3,70 @@
 namespace directapi\tests\unit;
 
 
+use directapi\common\criterias\IdsCriteria;
 use directapi\DirectApiService;
+use directapi\services\adgroups\criterias\AdGroupsSelectionCriteria;
+use directapi\services\adgroups\enum\AdGroupFieldEnum;
 use directapi\services\adgroups\models\AdGroupAddItem;
+use directapi\services\adgroups\models\AdGroupUpdateItem;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * @property \directapi\services\adgroups\AdGroupsService adGroupsService
+ */
 class AdGroupsServiceTest extends TestCase
 {
-    public function __construct($name = null, array $data = [], $dataName = ''){
+    public function __construct($name = null, array $data = [], $dataName = '')
+    {
         parent::__construct($name, $data, $dataName);
         $directApiService = new DirectApiService(DIRECT_UPDATE_ACCESS_TOKEN, DIRECT_UPDATE_LOGIN);
-
         $this->adGroupsService = $directApiService->getAdGroupsService();
     }
 
-    public function testAdd(){
+    public function testAdd()
+    {
+
         $adGroup = new AdGroupAddItem();
-        $adGroup->Name='Èìÿ';
-        $adGroup->RegionIds=174;
+        $adGroup->Name = 'Test';
+        $adGroup->CampaignId = CAMPAIGH_ID;
+        $adGroup->RegionIds = [1];
+        $result = $this->adGroupsService->add([$adGroup]);
+        $this->assertNotEmpty($result);
 
-
+        foreach ($result as $actionResult) {
+            var_dump($actionResult->Id);
+        }
     }
 
-    public function testDelete(){
+    public function testDelete()
+    {
+        $deleteGroup = new IdsCriteria();
+        $deleteGroup->Ids = [GROUPId];
 
+        $deleteResult = $this->adGroupsService->delete($deleteGroup);
+        $this->assertNotEmpty($deleteResult);
+
+        var_dump($deleteResult);
     }
 
-    public function testGet(){
+    public function testGet()
+    {
+        $SelectionCriteria = new AdGroupsSelectionCriteria;
+        $SelectionCriteria->Ids = [GROUPId];
+        $response = $this->adGroupsService->get($SelectionCriteria, AdGroupFieldEnum::getValues());
+        $this->assertNotEmpty($response);
 
+        var_dump($response);
     }
 
-    public function testUpdate(){
-
+    public function testUpdate()
+    {
+        $updateGroup = new AdGroupUpdateItem();
+        $updateGroup->Id = GROUPId;
+        $updateGroup->Name = 'CHANGED';
+        $updateGroup->RegionIds = [1];
+        $response = $this->adGroupsService->update([$updateGroup]);
+        $this->assertNotEmpty($response);
+        var_dump($response);
     }
 }
