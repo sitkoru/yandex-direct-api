93d684f48e094ef5d9d00fb28400e52661e793f9
KateAAAA <a.katyxa@mail.ru>
2017-08-14 17:38:07 +0500

    tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,3 +7,4 @@ $loader = require __DIR__ . '/../vendor/autoload.php';
 AnnotationRegistry::registerLoader([$loader, 'loadClass']);
 
 require_once __DIR__ . '/constants.php';
+require_once __DIR__ . '/constants.dist.php';
