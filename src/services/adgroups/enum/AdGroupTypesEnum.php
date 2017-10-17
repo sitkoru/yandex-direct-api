33b83dbcf79be69fd1a05ba5096a8b5672fc79ab
SonicGD <sonicgd@gmail.com>
2017-10-17 10:02:57 +0500

    add missing enum value

--- src/services/adgroups/enum/AdGroupTypesEnum.php
+++ src/services/adgroups/enum/AdGroupTypesEnum.php
@@ -9,4 +9,5 @@ class AdGroupTypesEnum extends Enum
 {
     const TEXT_AD_GROUP = 'TEXT_AD_GROUP';
     const MOBILE_APP_AD_GROUP = 'MOBILE_APP_AD_GROUP';
+    const DYNAMIC_TEXT_AD_GROUP = 'DYNAMIC_TEXT_AD_GROUP';
 }
