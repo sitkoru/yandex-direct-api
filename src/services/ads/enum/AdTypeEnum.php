1a4924c18dbf002c4a2540767da2b946a7eb1fd4
SonicGD <sonicgd@gmail.com>
2017-11-27 09:56:55 +0500

    Add IMAGE_AD to AdTypeEnum

--- src/services/ads/enum/AdTypeEnum.php
+++ src/services/ads/enum/AdTypeEnum.php
@@ -9,4 +9,5 @@ class AdTypeEnum extends Enum
     const TEXT_AD = 'TEXT_AD';
     const MOBILE_APP_AD = 'MOBILE_APP_AD';
     const DYNAMIC_TEXT_AD = 'DYNAMIC_TEXT_AD';
+    const IMAGE_AD = 'IMAGE_AD';
 }
