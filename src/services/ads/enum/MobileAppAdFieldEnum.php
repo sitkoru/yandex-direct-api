5bcd4ed1bd419da633349ef31141ea36dbfa99cd
SonicGD <sonicgd@gmail.com>
2017-12-26 16:55:25 +0500

    Fix enums

--- src/services/ads/enum/MobileAppAdFieldEnum.php
+++ src/services/ads/enum/MobileAppAdFieldEnum.php
@@ -13,4 +13,9 @@ class MobileAppAdFieldEnum extends Enum
     const ACTION = 'Action';
     const AD_IMAGE_HASH = 'AdImageHash';
     const AD_IMAGE_MODERATION = 'AdImageModeration';
+    const VCARD_ID = 'VCardId';
+    const VCARD_MODERATION = 'VCardModeration';
+    const SITELINK_SET_ID = 'SitelinkSetId';
+    const SITELINKS_MODERATION = 'SitelinksModeration';
+    const AD_EXTENSIONS = 'AdExtensions';
 }
--- src/services/ads/enum/TextAdFieldEnum.php
+++ src/services/ads/enum/TextAdFieldEnum.php
@@ -7,13 +7,12 @@ use directapi\components\Enum;
 class TextAdFieldEnum extends Enum
 {
     const TITLE = 'Title';
-    const TITLE2 = 'Title';
+    const TITLE2 = 'Title2';
     const TEXT = 'Text';
     const HREF = 'Href';
     const DISPLAY_DOMAIN = 'DisplayDomain';
     const DISPLAY_URL_PATH = 'DisplayUrlPath';
     const DISPLAY_URL_PATH_MODERATION = 'DisplayUrlPathModeration';
-    const MOBILE = 'Mobile';
     const VCARD_ID = 'VCardId';
     const SITE_LINK_SET_ID = 'SitelinkSetId';
     const AD_IMAGE_HASH = 'AdImageHash';
