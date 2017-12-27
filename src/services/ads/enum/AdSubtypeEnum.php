fcd7cf3889e30b7cffd3849cb06c673727a17c8b
SonicGD <sonicgd@gmail.com>
2017-12-27 11:58:50 +0500

    Fix ad subtypes

--- src/services/ads/enum/AdSubtypeEnum.php
+++ src/services/ads/enum/AdSubtypeEnum.php
@@ -9,4 +9,8 @@ class AdSubtypeEnum extends Enum
 {
     const NONE = 'NONE';
     const TEXT_IMAGE_AD = 'TEXT_IMAGE_AD';
+    const TEXT_AD_BUILDER_AD = 'TEXT_AD_BUILDER_AD';
+    const MOBILE_APP_IMAGE_AD = 'MOBILE_APP_IMAGE_AD';
+    const TEXT_AD_BUILDER_AD = 'TEXT_AD_BUILDER_AD';
+    const MOBILE_APP_AD_BUILDER_AD = 'MOBILE_APP_AD_BUILDER_AD';
 }
