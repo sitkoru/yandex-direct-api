7f63e0666ecef031c629d8b5f73e72dd823d8264
katyxa <a.katyxa@sitko.ru>
2018-04-18 17:00:54 +0500

    обработка 1002 ошибки

--- src/DirectApiService.php
+++ src/DirectApiService.php
@@ -432,7 +432,7 @@ class DirectApiService
                 throw new DirectApiException('Ошибка при подключении к яндексу: ' . $exception->getMessage() . ' Code: ' . $exception->getCode());
             }
         } catch (RequestException $exception) {
-            throw new DirectApiException('Ошибка при отправке запроса к яндексу: ' . $exception->getMessage() . '. Response: ' . $exception->getResponse()->getBody()->getContents() . ' Code: ' . $exception->getCode());
+            throw new DirectApiException('Ошибка при отправке запроса к яндексу: ' . $exception->getMessage() . '. Response: ' . $exception->getResponse()->getBody()->getContents() . ' Code: ' . $exception->getCode(), 0, null, $exception->getResponse()->getBody()->getContents());
         } catch (\Throwable $exception) {
             throw new DirectApiException('Ошибка при запросе к яндексу' . $exception->getMessage() . ' Code: ' . $exception->getCode());
         }
