d85f851fe83f441952641a656bdabdba099a54dc
SonicGD <sonicgd@gmail.com>
2018-10-24 17:14:40 +0500

    Fix constants values

--- src/services/campaigns/enum/CampaignTypeEnum.php
+++ src/services/campaigns/enum/CampaignTypeEnum.php
@@ -7,7 +7,7 @@ use directapi\components\Enum;
 class CampaignTypeEnum extends Enum
 {
     public const TEXT_CAMPAIGN = 'TEXT_CAMPAIGN';
-    public const MOBILE_APP_CAMPAIGN = 'MOBILE_APP_CAMPAIGN ';
-    public const DYNAMIC_TEXT_CAMPAIGN = 'DYNAMIC_TEXT_CAMPAIGN ';
-    public const CPM_BANNER_CAMPAIGN = 'CPM_BANNER_CAMPAIGN ';
+    public const MOBILE_APP_CAMPAIGN = 'MOBILE_APP_CAMPAIGN';
+    public const DYNAMIC_TEXT_CAMPAIGN = 'DYNAMIC_TEXT_CAMPAIGN';
+    public const CPM_BANNER_CAMPAIGN = 'CPM_BANNER_CAMPAIGN';
 }
