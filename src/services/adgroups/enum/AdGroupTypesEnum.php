745ac3f14bf56dbe949dd84ddb3943eca088257b
Nadia <nadia__@bk.ru>
2019-01-24 16:42:31 +0500

    фвв

--- src/services/adgroups/enum/AdGroupTypesEnum.php
+++ src/services/adgroups/enum/AdGroupTypesEnum.php
@@ -10,4 +10,5 @@ class AdGroupTypesEnum extends Enum
     public const TEXT_AD_GROUP = 'TEXT_AD_GROUP';
     public const MOBILE_APP_AD_GROUP = 'MOBILE_APP_AD_GROUP';
     public const DYNAMIC_TEXT_AD_GROUP = 'DYNAMIC_TEXT_AD_GROUP';
+    public const CPM_BANNER_AD_GROUP = 'CPM_BANNER_AD_GROUP';
 }
