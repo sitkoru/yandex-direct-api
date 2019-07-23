ec8134ce4faa6afd199455e7c9899cc2ad7f9f2c
George <sonicgd@gmail.com>
2019-07-23 12:31:35 +0500

    Add ad fields enums and parameters

--- src/services/ads/enum/CpcVideoAdBuilderAdFieldEnum.php
+++ src/services/ads/enum/CpcVideoAdBuilderAdFieldEnum.php
@@ -1,11 +1,12 @@
 <?php
 
+
 namespace directapi\services\ads\enum;
 
 
 use directapi\components\Enum;
 
-class TextAdBuilderAdFieldEnum extends Enum
+class CpcVideoAdBuilderAdFieldEnum extends Enum
 {
     public const CREATIVE = 'Creative';
     public const HREF = 'Href';
