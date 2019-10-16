684ebfb7cdf84f08fda1d5b4337a3e4bc287089e
Oblezov <delcom@ya.ru>
2019-10-16 19:17:21 +0400

    Add field Operation for update Priority goals

---
+++ src/services/campaigns/models/PriorityGoalsUpdateSetting.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace directapi\services\campaigns\models;
+
+use directapi\components\Model;
+
+class PriorityGoalsUpdateSetting extends Model
+{
+
+    /**
+     * @var PriorityGoalsUpdateItem[]
+     */
+    public $Items = [];
+}
