93552bcdd9177a86d3528c7c291e54bc05617f97
Sergey Nechaev <slow_cheetah@inbox.ru>
2020-02-19 17:17:25 +0700

    Ad statuses

--- src/services/ads/enum/AdStatusEnum.php
+++ src/services/ads/enum/AdStatusEnum.php
@@ -7,5 +7,8 @@ use directapi\common\enum\StatusEnum;
 class AdStatusEnum extends StatusEnum
 {
     public const DRAFT = 'DRAFT';
+    public const MODERATION = 'MODERATION';
     public const PREACCEPTED = 'PREACCEPTED';
+    public const ACCEPTED = 'ACCEPTED';
+    public const REJECTED = 'REJECTED';
 }
