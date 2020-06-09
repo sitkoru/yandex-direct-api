bad4169851b2db1ee3876527c9b39364415317fa
George Drak <sonicgd@gmail.com>
2020-06-09 15:49:21 +0500

    Fix enum

--- src/services/campaigns/enum/SmartCampaignFieldEnum.php
+++ src/services/campaigns/enum/SmartCampaignFieldEnum.php
@@ -7,7 +7,7 @@ use directapi\components\Enum;
 
 class SmartCampaignFieldEnum extends Enum
 {
-    public const COUNTER_IDS = 'CounterIds';
+    public const COUNTER_ID = 'CounterId';
     public const SETTINGS = 'Settings';
     public const BIDDING_STRATEGY = 'BiddingStrategy';
     public const PRIORITY_GOALS = 'PriorityGoals';
