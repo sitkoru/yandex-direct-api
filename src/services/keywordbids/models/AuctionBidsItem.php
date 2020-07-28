86980484e6d065c78a49e0456450a2422ae9f453
Puld <mr.puld@gmail.com>
2020-07-28 13:22:51 +0300

    style fixes

--- src/services/keywordbids/models/AuctionBidsItem.php
+++ src/services/keywordbids/models/AuctionBidsItem.php
@@ -6,10 +6,10 @@ use directapi\components\Model;
 
 class AuctionBidsItem extends Model
 {
-	/**
-	 * Массив ставок и списываемых цен на поиске, соответствующих различным объемам трафика.
-	 *
-	 * @var AuctionKeywordBidItem[]
-	 */
-	public $AuctionBidItems;
+    /**
+     * Массив ставок и списываемых цен на поиске, соответствующих различным объемам трафика.
+     *
+     * @var AuctionKeywordBidItem[]
+     */
+    public $AuctionBidItems;
 }
